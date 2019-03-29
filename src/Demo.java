98514f9e5883492222e8d2262cd755271d6f755b
ZhaoLeiMing <373433427@qq.com>
2019-03-29 16:57:32 +0800

    Initial commit

---
+++ src/Demo.java
@@ -0,0 +1,12 @@
+/**
+ * FileName: Demo
+ * Author:   赵雷鸣
+ * Date:     2019/3/29 16:26
+ * Description: test
+ */
+
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
